c6fdbb08303075c5e0f024eb2e65a06d93fb4e90
oapmartins <octaviomartins10@gmail.com>
2022-02-07 00:10:33 -0300

    Iniciando tela de pagamento por pix

--- lib/app/modules/order/finished/finished_page.dart
+++ lib/app/modules/order/finished/finished_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:vakinha_burger/app/core/ui/widgets/vakinha_button.dart';
 
 class FinishedPage extends StatelessWidget {
   const FinishedPage({Key? key}) : super(key: key);
@@ -13,10 +15,41 @@ class FinishedPage extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               mainAxisSize: MainAxisSize.min,
               children: [
-                Image.asset('assets/images/logo_rounded.png'),
+                Spacer(),
+                Image.asset(
+                  'assets/images/logo_rounded.png',
+                  width: context.widthTransformer(reducedBy: 50),
+                  height: context.heightTransformer(reducedBy: 70),
+                ),
                 const SizedBox(height: 10),
-                const Text(
-                  'Pedido realizado com sucesso, clique no botão abaixo para acessar o QRCode do pix',
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text(
+                    'Pedido realizado com sucesso, clique no botão abaixo para acessar o QRCode do pix',
+                    textAlign: TextAlign.center,
+                    style: context.textTheme.subtitle1?.copyWith(
+                      color: context.theme.primaryColorDark,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                SizedBox(
+                  width: context.widthTransformer(reducedBy: 30),
+                  child: VakinhaButton(
+                    label: 'PIX',
+                    color: context.theme.primaryColorDark,
+                    onpressed: () {},
+                  ),
+                ),
+                Spacer(),
+                Container(
+                  margin: EdgeInsets.only(bottom: 20),
+                  width: context.widthTransformer(reducedBy: 10),
+                  child: VakinhaButton(
+                    label: 'FECHAR',
+                    onpressed: () {},
+                  ),
                 ),
               ],
             ),
